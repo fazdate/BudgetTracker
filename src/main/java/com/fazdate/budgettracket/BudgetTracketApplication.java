d00ffb0d3c41384b66358e3c35f282b8ba557311
Fazekas Dániel <septumofficial@gmail.com>
2022-11-13 14:49:05 +0100

    Initial commit

---
+++ src/main/java/com/fazdate/budgettracket/BudgetTracketApplication.java
@@ -0,0 +1,13 @@
+package com.fazdate.budgettracket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetTracketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BudgetTracketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fazdate/budgettracket/BudgetTracketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fazdate.budgettracket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetTracketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
